4478ae0428d9da67f1e1fa514de1ef9175b08690
Nicolás Vargas Cabrera <splattcol@gmail.com>
2016-08-23 18:07:39 -0500

    modifica boundary

---
+++ OpenFOAM/changeBoundary.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pyFoamChangeBoundaryType.py . FrontAndBack empty
+pyFoamChangeBoundaryType.py . patch1 wall
+pyFoamChangeBoundaryType.py . patch2 wall
+pyFoamChangeBoundaryType.py . patch2 wall
+pyFoamChangeBoundaryType.py . Shaft wall
+pyFoamChangeBoundaryType.py . Lat-Wall wall
+pyFoamChangeBoundaryType.py . AMI-St cyclicAMI
+pyFoamChangeBoundaryType.py . AMI-Rt cyclicAMI
+
